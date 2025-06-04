c1ab034fd25aa3a518efae2a461c25bd272b62c8
jupitters <sampaiobale@gmail.com>
2025-06-04 17:47:05 -0300

    mouse movement

--- object_renderer.py
+++ object_renderer.py
@@ -7,7 +7,7 @@ class ObjectRenderer:
         self.game = game
         self.screen = game.screen
         self.wall_textures = self.load_wall_textures()
-        #self.sky_image = self.get_texture('resources/textures/sky.png', (WIDTH, HALF_HEIGHT))
+        self.sky_image = self.get_texture('resources/textures/ceiling.png', (WIDTH, HALF_HEIGHT))
         self.sky_offset = 0
         #self.blood_screen = self.get_texture('resources/textures/blood_screen.png', RES)
         self.digit_size = 90
@@ -18,15 +18,15 @@ class ObjectRenderer:
         #self.win_image = self.get_texture('resources/textures/win.png', RES)
 
     def draw(self):
-        #self.draw_background()
+        self.draw_background()
         self.render_game_objects()
 
-    # def draw_background(self):
-    #     self.sky_offset = (self.sky_offset + 4.5 * self.game.player.rel) % WIDTH
-    #     self.screen.blit(self.sky_image, (-self.sky_offset, 0))
-    #     self.screen.blit(self.sky_image, (-self.sky_offset + WIDTH, 0))
-    #     # floor
-    #     pg.draw.rect(self.screen, FLOOR_COLOR, (0, HALF_HEIGHT, WIDTH, HEIGHT))
+    def draw_background(self):
+        self.sky_offset = (self.sky_offset + 4.5 * self.game.player.rel) % WIDTH
+        self.screen.blit(self.sky_image, (-self.sky_offset, 0))
+        self.screen.blit(self.sky_image, (-self.sky_offset + WIDTH, 0))
+        # floor
+        pg.draw.rect(self.screen, FLOOR_COLOR, (0, HALF_HEIGHT, WIDTH, HEIGHT))
 
     def render_game_objects(self):
         list_objects = sorted(self.game.raycasting.objects_to_render, key=lambda t: t[0], reverse=True)
