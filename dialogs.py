8f1e7656be47b55f891aa532c1553725939cb68e
jupitters <sampaiobale@gmail.com>
2025-06-22 10:50:55 -0300

    fix dialog box

--- dialogs.py
+++ dialogs.py
@@ -1,15 +1,14 @@
 from sprite_object import *
 class Dialogs(AnimatedSprite):
-    def __init__(self, game, path='resources/sprites/dialogs/atendente/dialogo.png', scale=0.5, pos=(6, 13)):
-        super().__init__(game=game, path=path, scale=scale, pos=pos)
-        self.pos = pos
+    def __init__(self, game, path='resources/sprites/dialogs/atendente/dialogo.png', scale=0.5):
+        super().__init__(game=game, path=path, scale=scale)
         self.images = deque(
             [pg.transform.smoothscale(img, (self.image.get_width() * scale, self.image.get_height() * scale))
              for img in self.images])
         self.dialogo = (HALF_WIDTH - self.images[0].get_width() // 2, HEIGHT * 1.2 - self.images[0].get_height())
 
     def draw(self):
-        if not self.player.holding_ticket and (self.game.player.map_pos == self.pos or self.game.player.map_pos == (7.5, 13)):
+        if not self.player.holding_ticket and (self.game.player.map_pos == (6, 13) or self.game.player.map_pos == (7, 13)):
             self.game.screen.blit(self.images[0], self.dialogo)
         else:
             return
